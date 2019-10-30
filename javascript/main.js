06a6a60edcc6dc7f5818536b7bf70e03d4254cea
Endermax <Endermax200113@mail.ru>
2019-10-30 23:07:46 +0300

    Диалоговое окно confirm

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,14 @@
 document.write('<div class="ind"></div>');
-document.write('<p id="not">Нажмите 1 из 2-х вариантов</p>');
-document.write('<p id="yes">Да</p>');
-document.write('<p id="no">Нет</p>');
+document.write('<p id="clicker">Нажми меня</p>');
 
 document.addEventListener("click", function(e) {
-	if (e.target == document.querySelector("#yes")) alert('Вы нажали текст "Да"!');
-	else if (e.target == document.querySelector("#no")) alert('Вы нажали текст "Нет"!');
-	else if (e.target == document.querySelector("#not")) alert('Вы нажали не тот текст! ;)');
+	var element = e.target;
+	var clicker = document.querySelector("#clicker");
+
+	if (element == clicker) {
+		if (confirm("Нажмите любую кнопку")) alert('Вы нажали кнопку "Ок"');
+		else alert('Вы нажали кнопку "Отмена"');
+
+		return;
+	}
 });
