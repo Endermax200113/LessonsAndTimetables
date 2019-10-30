e02603238b27d87b34b8aa0537c122a0e24ffb26
Endermax <Endermax200113@mail.ru>
2019-10-30 21:21:59 +0300

    Обработка клика на текст в js

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,11 @@
-document.write("<a href=''>Привет мир</a>");
+document.write('<div class="ind"></div>');
+document.write('<div class="ind"></div>');
+document.write('<p id="not">Нажмите 1 из 2-х вариантов</p>');
+document.write('<p id="yes">Да</p>');
+document.write('<p id="no">Нет</p>');
+
+document.addEventListener("click", function(e) {
+	if (e.target == document.querySelector("#yes")) alert('Вы нажали текст "Да"!');
+	else if (e.target == document.querySelector("#no")) alert('Вы нажали текст "Нет"!');
+	else if (e.target == document.querySelector("#not")) alert('Вы нажали не тот текст! ;)');
+});
