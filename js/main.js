be6ce47b385cad9ff0c900c1af4e7dff3adbc763
Endermax <Endermax200113@mail.ru>
2019-12-08 19:57:19 +0300

    Сделан .header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
-var version = "0.0.0.2";
+var version = "0.0.0.3";
+
+var headerMenu = fromElement(".headerLeftLinkMenu");
+var header = fromElement(".header");
+var menuOpen = false;
 
 window.addEventListener("load", function(e) {
 	console.info("Версия сайта: " + version);
+});
+
+document.addEventListener("click", function(e) {
+	let element = e.target
+
+	if (!isEmpty(headerMenu) && equalsElements(element, headerMenu)) {
+		if (!menuOpen) {
+			setStyleEl(header, "height", "145px");
+			menuOpen = true;
+		} else {
+			setStyleEl(header, "height", "35px");
+			menuOpen = false;
+		}
+	}
+});
+
+window.addEventListener("resize", function(e) {
+	if (!isEmpty(headerMenu)) {
+		if (!mobVersion) {
+			setStyleEl(header, "height", "70px");
+			menuOpen = false;
+		} else {
+			setStyleEl(header, "height", "35px");
+			menuOpen = false;
+		}
+	}
 });
