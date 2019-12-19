4450f716ad013f7b528b36d91398caceacf07bc5
Endermax <Endermax200113@mail.ru>
2019-12-19 13:31:26 +0300

    Экстренная замена

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var version = "0.0.3.38";
+var version = "0.0.3.39";
 
 var headerMenu = fromElement(".headerLeftLinkMenu");
 var header = fromElement(".header");
---
+++ pages/news/process.js
@@ -0,0 +1,53 @@
+var newsLast = fromElement("#newsLast");
+var newsCollege = fromElement("#newsCollege");
+var newsGroup = fromElement("#newsGroup");
+var newsSite = fromElement("#newsSite");
+
+//last
+{
+	var countNewsLast = news[0].count;
+	var countNewsCollege = college[0].count;
+	var countNewsGroup = group[0].count;
+	var countNewsSite = site[0].count;
+	var dataCollege = infoCollege;
+	var dataGroup = infoGroup;
+	var dataSite = infoSite;
+	let j = 0;
+
+	for (let i = countNewsCollege - 1; i >= 0; i--) {
+		
+	}
+
+	for (let i = countNewsGroup - 1; i >= 0; i--) {
+
+	}
+
+	for (let i = countNewsSite - 1; i >= 0; i--) {
+		let title = dataSite[i].title;
+		let important = dataSite[i].important;
+		let number = dataSite[i].number;
+		let glNumber = dataSite[i].glNumber;
+		let day = dataSite[i].day;
+		let month = dataSite[i].month;
+		let year = dataSite[i].year;
+
+		
+
+		j++;
+	}
+}
+
+//college
+{
+
+}
+
+//group
+{
+
+}
+
+//site
+{
+
+}
