fc59434d1330a2be351063a816f47deb5fe4a160
Endermax <Endermax200113@mail.ru>
2019-12-25 16:34:54 +0300

    Замена

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var version = "0.0.3.45";
+var version = "0.0.3.46";
 
 var headerMenu = fromElement(".headerLeftLinkMenu");
 var header = fromElement(".header");
--- pages/news/process.js
+++ pages/news/process.js
@@ -3,93 +3,128 @@ var newsCollege = fromElement("#newsCollege");
 var newsGroup = fromElement("#newsGroup");
 var newsSite = fromElement("#newsSite");
 
-//last
-{
-	var dataCollege = infoCollege;
-	var dataGroup = infoGroup;
-	var dataSite = infoSite;
-	let arrNews = [];
-	let newArrNews = [];
-	let sortArrNews = [];
+var dataCollege = infoCollege;
+var dataGroup = infoGroup;
+var dataSite = infoSite;
+var arrNews = [];
+var sortArrNews = [];
 
-	for (let i = 0; i < dataCollege.length; i++) {
-		let title = dataCollege[i].title;
-		let important = dataCollege[i].important;
-		let number = dataCollege[i].number;
-		let glNumber = dataCollege[i].glNumber;
-		let day = dataCollege[i].day;
-		let month = dataCollege[i].month;
+for (let i = 0; i < dataCollege.length; i++) {
+	let title = dataCollege[i].title;
+	let important = dataCollege[i].important;
+	let glNumber = dataCollege[i].glNumber;
+	let day = dataCollege[i].day;
+	let month = dataCollege[i].month;
 
-		arrNews[arrNews.length] = {
-			glNumber: glNumber,
-			numberArr: i,
-			type: "college"
-		};
-	}
+	arrNews[arrNews.length] = {
+		glNumber: glNumber,
+		title: title,
+		important: important,
+		day: day,
+		month: month
+	};
+}
 
-	for (let i = 0; i < dataGroup.length; i++) {
-		let title = dataGroup[i].title;
-		let important = dataGroup[i].important;
-		let number = dataGroup[i].number;
-		let glNumber = dataGroup[i].glNumber;
-		let day = dataGroup[i].day;
-		let month = dataGroup[i].month;
+for (let i = 0; i < dataGroup.length; i++) {
+	let title = dataGroup[i].title;
+	let important = dataGroup[i].important;
+	let glNumber = dataGroup[i].glNumber;
+	let day = dataGroup[i].day;
+	let month = dataGroup[i].month;
 
-		arrNews[arrNews.length] = {
-			glNumber: glNumber,
-			numberArr: i,
-			type: "group"
-		};
-	}
+	arrNews[arrNews.length] = {
+		glNumber: glNumber,
+		title: title,
+		important: important,
+		day: day,
+		month: month
+	};
+}
 
-	for (let i = 0; i < dataSite.length; i++) {
-		let title = dataSite[i].title;
-		let important = dataSite[i].important;
-		let number = dataSite[i].number;
-		let glNumber = dataSite[i].glNumber;
-		let day = dataSite[i].day;
-		let month = dataSite[i].month;
+for (let i = 0; i < dataSite.length; i++) {
+	let title = dataSite[i].title;
+	let important = dataSite[i].important;
+	let glNumber = dataSite[i].glNumber;
+	let day = dataSite[i].day;
+	let month = dataSite[i].month;
 
-		arrNews[arrNews.length] = {
-			glNumber: glNumber,
-			numberArr: i,
-			type: "site"
-		}
-	}
-
-	let count = dataSite.length + dataGroup.length + dataCollege.length;
-	for (let i = 0; i < count; i++) {
-		newArrNews[i] = i;
-		console.log(i);
-	}
-
-	for (let i = 0; i < count; i++){
-		find(arrNews, i);
-	}
-
-	for (let el of arrNews) {
-		console.log(el.glNumber);
-		console.log(el.numberArr);
-		console.log(el.type);
-		console.log(" ");
+	arrNews[arrNews.length] = {
+		glNumber: glNumber,
+		title: title,
+		important: important,
+		day: day,
+		month: month
 	}
 }
 
-//college
-{
-
+let count = dataSite.length + dataGroup.length + dataCollege.length;
+for (let i = 0; i < count; i++) {
+	sortArrNews[i] = find(arrNews, i + 1);
 }
 
-//group
-{
+let countLast = 0;
+for (let i = count - 1; i >= 0; i--) {
+	if (countLast == 7) {
+		newsLast.innerHTML +=
+		'<div class="contentBlockDataNewsButton">' +
+			'<div class="contentBlockDataNewsButtonOther">' +
+				"Посмотреть все новости" + 
+			"</div>" +
+		"</div>";
 
+		break;
+	}
+
+	let data = sortArrNews[i];
+
+	if (!data.important) {
+		newsLast.innerHTML += 
+		'<div class="contentBlockDataNewsButton">' + 
+			'<div class="contentBlockDataNewsButtonDate">' +
+				data.day + " " + parseMonth(data.month) +
+			"</div>" + 
+			'<div class="contentBlockDataNewsButtonTitle">' +
+				data.title + 
+			"</div>" +
+		"</div>";
+	} else {
+		newsLast.innerHTML += 
+		'<div class="contentBlockDataNewsButton">' + 
+			'<div class="contentBlockDataNewsButtonDate">' +
+				data.day + " " + parseMonth(data.month) +
+			"</div>" + 
+			'<div class="contentBlockDataNewsButtonTitle">' +
+				data.title + 
+			"</div>" +
+			'<div class="contentBlockDataNewsButtonImp">' +
+				"Важное!"
+			"</div>" +
+		"</div>";
+	} 
+
+	countLast++;
 }
 
-//site
-{
 
+
+function find(arr, number) {
+	for (let el of arr) {
+		if (el.glNumber == number) return el;
+	}
 }
 
-function find(arr[], number) {
-
+function parseMonth(number) {
+	if (number == 1) return "янв";
+	else if (number == 2) return "фев";
+	else if (number == 3) return "мар";
+	else if (number == 4) return "апр";
+	else if (number == 5) return "май";
+	else if (number == 6) return "июн";
+	else if (number == 7) return "июл";
+	else if (number == 8) return "авг";
+	else if (number == 9) return "сен";
+	else if (number == 10) return "окт";
+	else if (number == 11) return "ноя";
+	else if (number == 12) return "дек";
+	else return null;
 }
